afcd27c2e9c68712b0eee246783da30a82c80255
AcresGuz <alberto.crespo86@gmail.com>
2020-03-23 13:45:35 +0100

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,5 +1,5 @@
 class Constants:
-    MONGODB_URL = "mongodb+srv://covid:123123123!@cluster0-juygf.mongodb.net/test?retryWrites=true&w=majority"
+    MONGODB_URL = "mongodb+srv://covid:XXXXXX@cluster0-juygf.mongodb.net/test?retryWrites=true&w=majority"
     TRACKS = ['codiv', 'coronavirus', 'covid19']
     LANGUAGES = ["es"]
 
